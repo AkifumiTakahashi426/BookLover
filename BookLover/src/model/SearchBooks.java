990b85b80a4e39b97dcc2ffd81701b4b16a04bf1
81907 <81907@DESKTOP-7KUU7BR>
2021-03-08 18:36:32 +0900

    本の検索用にモデル作ったよ

--- BookLover/src/model/SearchBooks.java
+++ BookLover/src/model/SearchBooks.java
@@ -9,7 +9,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
